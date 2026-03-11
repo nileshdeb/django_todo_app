80e7e6422e56742fbfd25fdc73df9264a4263b24
Nilesh Deb <debnilesh69@gmail.com>
2026-03-11 11:34:36 +0530

    add build.sh and correct requirements.txt

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -o errexit
+
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+
+cd todoproject
+
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+
